cd69fbd34c4f88d25f03e74418e26d4607904808
Stan Drozd <stan@pyth.network>
2023-02-09 18:34:36 +0100

    drozdziak1/k7-hotswap ansi: Make colemak default

--- keyboards/keychron/k7/keymaps/ansi/keymap.c
+++ keyboards/keychron/k7/keymaps/ansi/keymap.c
@@ -21,8 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 enum layer_names {
-    _BASE,
-    _CLMK,
+    _COLEMAK,
+    _QWERTY,
     _NAV,
     _FN1,
     _FN2,
@@ -30,24 +30,24 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
- /*      Col:         0                    1          2          3        4         5        6         7        8        9          10         11                   12         13          */
-      [_BASE] = { {   KC_GESC,             KC_1,      KC_2,      KC_3,    KC_4,     KC_5,    KC_6,     KC_7,    KC_8,    KC_9,      KC_0,      KC_MINS,              KC_EQL,    KC_BSPC    }, 
-                  {   KC_TAB,              KC_Q,      KC_W,      KC_E,    KC_R,     KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,              KC_RBRC,   KC_BSLASH  }, 
-                  {   LT(_NAV, KC_CAPS),   KC_A,      KC_S,      KC_D,    KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   LT(_NAV, KC_QUOT),    KC_NO,     KC_ENT     }, 
-                  {   KC_LSFT,             KC_NO,     KC_Z,      KC_X,    KC_C,     KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,              KC_RSFT,   KC_UP      }, 
-                  {   KC_LCTL,             KC_LGUI,   KC_LALT,                               KC_SPC,                     KC_RALT,   MO(_FN1),  MO(_FN2),             KC_LEFT,   KC_DOWN,   KC_RGHT,   KC_PGDN,   KC_PGUP,   KC_HOME,   KC_DEL     }
+ /*         Col:         0                    1          2          3        4         5        6         7        8        9          10         11                   12         13          */
+      [_COLEMAK] = { {   KC_GESC,             KC_1,      KC_2,      KC_3,    KC_4,     KC_5,    KC_6,     KC_7,    KC_8,    KC_9,      KC_0,      KC_MINS,              KC_EQL,    KC_BSPC    }, 
+                     {   KC_TAB,              KC_Q,      KC_W,      KC_F,    KC_P,     KC_G,    KC_J,     KC_L,    KC_U,    KC_Y,      KC_SCLN,   KC_LBRC,              KC_RBRC,   KC_BSLASH  },   
+                     {   LT(_NAV, KC_CAPS),   KC_A,      KC_R,      KC_S,    KC_T,     KC_D,    KC_H,     KC_N,    KC_E,    KC_I,      KC_O,      LT(_NAV, KC_QUOT),    KC_NO,     KC_ENT     },   
+                     {   KC_LSFT,             KC_NO,     KC_Z,      KC_X,    KC_C,     KC_V,    KC_B,     KC_K,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,              KC_RSFT,   KC_UP      }, 
+                     {   KC_LCTL,             KC_LGUI,   KC_LALT,                               KC_SPC,                     KC_RALT,   MO(_FN1),  MO(_FN2),             KC_LEFT,   KC_DOWN,   KC_RGHT,   KC_PGDN,   KC_PGUP,   KC_HOME,   KC_DEL     }
                },
-      [_CLMK] = { {   KC_TRNS,             KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS    },   
-                  {   KC_TRNS,             KC_Q,      KC_W,      KC_F,    KC_P,     KC_G,    KC_J,     KC_L,    KC_U,    KC_Y,      KC_SCLN,   KC_TRNS,              KC_TRNS,   KC_TRNS    },   
-                  {   KC_TRNS,             KC_A,      KC_R,      KC_S,    KC_T,     KC_D,    KC_H,     KC_N,    KC_E,    KC_I,      KC_O,      KC_TRNS,              KC_TRNS,   KC_TRNS    },   
-                  {   KC_TRNS,             KC_TRNS,   KC_Z,      KC_X,    KC_C,     KC_V,    KC_B,     KC_K,    KC_M,    KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS    },   
-                  {   KC_TRNS,             KC_TRNS,   KC_TRNS,                               KC_TRNS,                    KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS     }
+      [_QWERTY] =  { {   KC_TRNS,             KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS    }, 
+                     {   KC_TRNS,             KC_Q,      KC_W,      KC_E,    KC_R,     KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_TRNS,              KC_TRNS,   KC_TRNS    }, 
+                     {   KC_TRNS,             KC_A,      KC_S,      KC_D,    KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_TRNS,              KC_TRNS,   KC_TRNS    }, 
+                     {   KC_TRNS,             KC_TRNS,   KC_Z,      KC_X,    KC_C,     KC_V,    KC_B,     KC_N,    KC_M,    KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS    }, 
+                     {   KC_TRNS,             KC_TRNS,   KC_TRNS,                               KC_TRNS,                    KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS     }
                },
-      [_NAV] = {  {   KC_GRV,              KC_F1,     KC_F2,     KC_F3,   KC_F4,    KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,     KC_F10,    KC_F11,               KC_F12,   KC_TRNS     },   
-                  {   KC_TRNS,             KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS     },   
-                  {   TO(_BASE),           KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT,   KC_TRNS,   TO(_CLMK),            KC_TRNS,   KC_TRNS     },   
-                  {   KC_TRNS,             KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS     },   
-                  {   KC_TRNS,             KC_TRNS,   KC_TRNS,                               KC_TRNS,                    KC_ENT,    KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS     }
+      [_NAV] =    {  {   KC_GRV,              KC_F1,     KC_F2,     KC_F3,   KC_F4,    KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,     KC_F10,    KC_F11,               KC_F12,    KC_TRNS     },   
+                     {   KC_TRNS,             KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS     },   
+                     {   TO(_QWERTY),         KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT,   KC_TRNS,   TO(_COLEMAK),         KC_TRNS,   KC_TRNS     },   
+                     {   KC_TRNS,             KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS     },   
+                     {   KC_TRNS,             KC_TRNS,   KC_TRNS,                               KC_TRNS,                    KC_ENT,    KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS     }
                },		           														             
       [_FN1] = {  {   KC_GRV,              KC_SLCK,   KC_PAUS,   KC_APP,  KC_NO,    RGB_VAD, RGB_VAI,  KC_MPRV, KC_MPLY, KC_MNXT,   KC_MUTE,   KC_VOLD,              KC_VOLU,   KC_NO    },
                   {   KC_NO,               RGB_M_P,   RGB_M_B,   RGB_M_R, RGB_M_SW, RGB_M_T, RGB_SPD,  RGB_SPI, KC_NO,   KC_NO,     KC_INS,    KC_DEL,               KC_END,    KC_NO    },
